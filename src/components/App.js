abc6d13c605e8797f123ed935a3327374c412c61
Karpov Yuri <yurick@bk.ru>
2022-04-23 13:01:03 +0300

    feat: Take out the ImagePopup component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import '../index.css';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
+import ImagePopup from '../components/ImagePopup';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Main />
 
       <Footer />
+
+      <ImagePopup />
 
       <article className="popup" id="popup-edit">
         <div className="popup__container popup__container_show_form">
@@ -63,14 +66,6 @@ function App() {
           <form className="popup__form" name="deleteForm" noValidate>
             <button type="submit" aria-label="Кнопка подтверждения удаления" className="popup__submit-button">Да</button>
           </form>
-          <button type="button" aria-label="Кнопка закрытия окна" className="popup__close-button fade-opacity"></button>
-        </div>
-      </article>
-
-      <article className="popup popup_overlay_dark" id="popup-image">
-        <div className="popup__container popup__container_show_image">
-          <img src="#" alt="" className="popup__image" />
-          <p className="popup__signature"></p>
           <button type="button" aria-label="Кнопка закрытия окна" className="popup__close-button fade-opacity"></button>
         </div>
       </article>
---
+++ src/components/ImagePopup.js
@@ -0,0 +1,13 @@
+function ImagePopup() {
+  return (
+    <article className="popup popup_overlay_dark" id="popup-image">
+      <div className="popup__container popup__container_show_image">
+        <img src="#" alt="" className="popup__image" />
+        <p className="popup__signature"></p>
+        <button type="button" aria-label="Кнопка закрытия окна" className="popup__close-button fade-opacity"></button>
+      </div>
+    </article>
+  );
+}
+
+export default ImagePopup;
