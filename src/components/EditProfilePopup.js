1d37cbd2ce1fb4db89eff3fbc9643e042031d96d
Karpov Yuri <yurick@bk.ru>
2022-05-12 11:20:29 +0300

    fix: Correct filling in the user data editing form

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -24,7 +24,7 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser, isRenderLoading }) {
   React.useEffect(() => {
     setName(currentUser.name);
     setDescription(currentUser.about);
-  }, [currentUser]);
+  }, [currentUser, isOpen]);
 
 
   // Установка имени пользователя
